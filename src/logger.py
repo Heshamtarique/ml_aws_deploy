c7d9cd3845eabdfb87d4e26bd51a2ba559d7e9bd
Hesham Mohammad Tarique <heshamtarique.jmi@gmail.com>
2024-05-26 16:19:06 +0530

    Loggi

--- src/logger.py
+++ src/logger.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import logging
 import os
 from datetime import datetime
@@ -20,22 +19,3 @@ logging.basicConfig(
 
 if __name__=="__main__":
     logging.info("Logging started: ")
-=======
-import logging
-import os
-from datetime import datetime
-
-LOG_FILE=f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
-logs_path=os.path.join(os.getcwd(),"logs",LOG_FILE)
-os.makedirs(logs_path,exist_ok=True)
-
-LOG_FILE_PATH=os.path.join(logs_path,LOG_FILE)
-
-logging.basicConfig(
-    filename=LOG_FILE_PATH,
-    format="[ %(asctime)s ] %(lineno)d %(name)s - %(levelname)s - %(message)s",
-    level=logging.INFO,
-
-
-)
->>>>>>> 132fb3e80db94fd55a3cf5464dc8a6200f84b5e8

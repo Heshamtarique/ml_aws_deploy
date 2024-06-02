47260d3f18906716adbb9dcb1c406590c6e15d3c
Hesham Mohammad Tarique <heshamtarique.jmi@gmail.com>
2024-06-02 15:24:11 +0530

    update in DataTrans

--- src/components/data_transformation.py
+++ src/components/data_transformation.py
@@ -28,6 +28,51 @@ class DataTransformation:
         
     def get_data_transformer_object(self):
         try:
-            pass
-        except:
-            pass
+            numerical_columns = ["writing_score", "reading_score"]   # numerical features
+            
+            # categorical features 
+            categorical_columns = [
+                "gender",
+                "race_ethnicity",
+                "parental_level_of_education",
+                "lunch",
+                "test_preparation_course",
+            ]
+            
+            # sklearn pipeline -- numerical pipeline
+            num_pipeline= Pipeline(                                    
+                steps=[
+                ("imputer",SimpleImputer(strategy="median")),   # imputing with median
+                ("scaler",StandardScaler())
+
+                ]
+            )
+            # categorical pipeline -- 
+            cat_pipeline=Pipeline(
+                # 3 steps below
+                steps=[
+                ("imputer",SimpleImputer(strategy="most_frequent")),    # categoricals imputed with most frequesnt -- mode
+                ("one_hot_encoder",OneHotEncoder()),                    # encoding the categoricals
+                ("scaler",StandardScaler(with_mean=False))              # scaling data
+                ]
+
+            )
+
+            logging.info(f"Categorical columns: {categorical_columns}")
+            logging.info(f"Numerical columns: {numerical_columns}")
+
+            preprocessor=ColumnTransformer(
+                [
+                ("num_pipeline",num_pipeline,numerical_columns),
+                ("cat_pipelines",cat_pipeline,categorical_columns)
+
+                ]
+
+
+            )
+
+            return preprocessor
+        
+        except Exception as e:
+            raise CustomException(e,sys)
+      
